1e7a11f828a0e140779146f2e7cb3ed5b6212452
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2020-05-11 07:03:18 -0400

    note edit

--- routes/notes.js
+++ routes/notes.js
@@ -21,4 +21,13 @@ router.post("/notes/delete/:id",async function(req,res){
   await Note.findByIdAndUpdate(req.params.id, {deleted: true});
   res.redirect("/notes")
 })
+router.get("/notes/edit/:id",async function(req,res){
+  note = await Note.findById(req.params.id)
+  res.render("notes/edit",{note})
+})
+router.post("/notes/edit/:id",async function(req,res){
+  const { name, date, time,location,description} = req.body;
+  note = await Note.findByIdAndUpdate(req.params.id,{name,date,time,location,description})
+  res.redirect("/notes")
+})
 module.exports = router
